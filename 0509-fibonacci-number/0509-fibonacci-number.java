d0b44af72748391242347d65e42c626a6fba2e45
Bhoomika Chourey <155342295+Bhoomikachourey@users.noreply.github.com>
2026-08-13 15:12:19 +0530

    Time: 0 ms (100%), Space: 42.4 MB (16.16%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        int f1=0;
+        int f2=1;
+        for(int i=1;i<=n;i++){
+            int temp=f1+f2;
+            f1=f2;
+            f2=temp;
+        }
+        return f1;
+    }
+}
