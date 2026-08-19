be4e2cfda3fd7f1a952c1df1c581a253b7ba189f
Bhoomika Chourey <155342295+Bhoomikachourey@users.noreply.github.com>
2026-08-20 03:23:28 +0530

    Time: 11 ms (82.91%), Space: 59.5 MB (53.17%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -0,0 +1,22 @@
+class Solution {
+    private static int func(int nums[], int k){
+        if(k<0) return 0;
+        int left=0;
+        int right=0;
+        int count=0;
+        int sum=0;
+        while(right<nums.length){
+            sum+=nums[right]%2;
+            while(sum>k){
+                sum-=nums[left]%2;
+                left++;
+            }
+            count+=right-left+1;
+            right++;
+        }
+        return count;
+    }
+    public int numberOfSubarrays(int[] nums, int k) {
+        return func(nums, k)-func(nums ,k-1);
+    }
+}
